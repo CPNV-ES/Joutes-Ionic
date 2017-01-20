6671c1ec8909eb631bb8e72d2b9a845b843bcfc7
SMartignier <stephanemartignier@gmail.com>
2017-01-20 08:53:59 +0100

    Add link to pool on match list in team view

--- src/pages/pool/pool.ts
+++ src/pages/pool/pool.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {PoolService} from "../../providers/pool-service";
 import {SharedDataService} from "../../providers/sharedData-service";
 
 /*
-  Generated class for the Pool page.
+ Generated class for the Pool page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
-  selector: 'page-pool',
-  templateUrl: 'pool.html'
+    selector: 'page-pool',
+    templateUrl: 'pool.html'
 })
 export class PoolPage {
 
@@ -20,16 +20,16 @@ export class PoolPage {
     public pool: any;
     public poolData: any = {};
 
-  constructor(public navCtrl: NavController, public navParam: NavParams, public poolProvider: PoolService, public sharedDataProvider: SharedDataService) {
-    this.event = sharedDataProvider.getCurrentEvent();
-    this.tournament = sharedDataProvider.getCurrentTournament();
-    this.pool = sharedDataProvider.getCurrentPool();
+    constructor(public navCtrl: NavController, public navParam: NavParams, public poolProvider: PoolService, public sharedDataProvider: SharedDataService) {
+        this.event = sharedDataProvider.getCurrentEvent();
+        this.tournament = sharedDataProvider.getCurrentTournament();
+        this.pool = sharedDataProvider.getCurrentPool();
 
-    this.poolProvider.getPool(this.event.id, this.tournament.id, this.pool.id).subscribe(data => this.poolData = data);
-  }
+        this.poolProvider.getPool(this.event.id, this.tournament.id, this.pool.id).subscribe(data => this.poolData = data);
+    }
 
-  ionViewDidLoad() {
+    ionViewDidLoad() {
 
-  }
+    }
 
 }
