67e7dd94504d3102534776072cdac361e1b2d485
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2018-12-03 10:45:18 +0100

    Add getall and create function

--- src/providers/endpoint/endpoint.ts
+++ src/providers/endpoint/endpoint.ts
@@ -1,10 +1,30 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Endpoint } from '../../models/endpoint';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class EndpointProvider {
 
-  constructor(public http: HttpClient) {
+  private endpoints: Array<Endpoint> = []
+  private keyName: string = 'endpoints'
+
+  constructor(private storage: Storage) {
+    // Todo: track error
+    this.getAll()
+  }
+
+  create(endpoint: Endpoint){
+    if (this.endpoints != null) {
+      this.endpoints.push(endpoint)
+    } else {
+      this.endpoints = [endpoint]
+    }
+    return this.storage.set(this.keyName,this.endpoints)
+  }
+
+  async getAll() {
+    this.endpoints = await this.storage.get(this.keyName)
+    return this.endpoints
   }
 
 }
