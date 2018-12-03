1a5654d4026769f480fa41b34da6d3e00318b74f
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2018-12-03 10:45:05 +0100

    Add error tracking

--- src/pages/create-endpoint/create-endpoint.ts
+++ src/pages/create-endpoint/create-endpoint.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Endpoint } from '../../models/endpoint'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EndpointProvider } from '../../providers/endpoint/endpoint'
 
 @Component({
   selector: 'page-create-endpoint',
@@ -11,7 +12,7 @@ export class CreateEndpointPage {
 
   private endpointForm: FormGroup
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private endpointProvider: EndpointProvider) {
     this.endpointForm = this.formBuilder.group({
       name: ['', Validators.compose([Validators.required])],
       address: ['', Validators.compose([Validators.required])]
@@ -21,8 +22,12 @@ export class CreateEndpointPage {
   createEndpoint() {
     if (this.endpointForm.valid) {
       // Validation ok
-      let endpoint = new Endpoint(this.endpointForm.value.name, this.endpointForm.value.address)
-      console.log(endpoint)
+      try {
+        this.endpointProvider.create(new Endpoint(this.endpointForm.value.name, this.endpointForm.value.address))
+      } catch (error) {
+        // Todo: Good error notif
+        console.error(error)
+      }
     }
   }
 }
