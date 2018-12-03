8f6b26865fc11ff844df98c6dc83a276d8a7ab01
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2018-12-03 10:12:14 +0100

    Add form for create endpoint

--- src/pages/create-endpoint/create-endpoint.ts
+++ src/pages/create-endpoint/create-endpoint.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Endpoint } from '../../models/endpoint'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-create-endpoint',
@@ -7,6 +9,20 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CreateEndpointPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private endpointForm: FormGroup
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+    this.endpointForm = this.formBuilder.group({
+      name: ['', Validators.compose([Validators.required])],
+      address: ['', Validators.compose([Validators.required])]
+    })
+  }
+
+  createEndpoint() {
+    if (this.endpointForm.valid) {
+      // Validation ok
+      let endpoint = new Endpoint(this.endpointForm.value.name, this.endpointForm.value.address)
+      console.log(endpoint)
+    }
   }
 }
