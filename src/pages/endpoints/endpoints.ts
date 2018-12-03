bd18096a5fb4bcdd07f1af8fc532532f63bd5ec8
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2018-12-03 09:35:55 +0100

    Add button for open create endpoint

--- src/pages/endpoints/endpoints.ts
+++ src/pages/endpoints/endpoints.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { CreateEndpointPage } from '../create-endpoint/create-endpoint';
 
 @Component({
   selector: 'page-endpoints',
@@ -8,6 +9,10 @@ import { NavController, NavParams } from 'ionic-angular';
 export class EndpointsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  openCreateEndpoint() {
+    this.navCtrl.push(CreateEndpointPage)
   }
 
 }
