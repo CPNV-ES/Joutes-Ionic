4dbf9fd23079e979248d767a502e7c49375682c8
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2018-12-07 12:01:27 +0100

    Change toast controller by custom toast

--- src/pages/create-endpoint/create-endpoint.ts
+++ src/pages/create-endpoint/create-endpoint.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { Endpoint } from '../../models/endpoint'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EndpointProvider } from '../../providers/endpoint/endpoint';
-import { CONSTANTS } from '../../app/constants';
+import { ToastCustom } from '../../components/toast-custom/toast-custom'
 
 @Component({
   selector: 'page-create-endpoint',
@@ -13,7 +13,7 @@ export class CreateEndpointPage {
 
   private endpointForm: FormGroup
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private endpointProvider: EndpointProvider, private toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private endpointProvider: EndpointProvider, private toastCustom: ToastCustom) {
     this.endpointForm = this.formBuilder.group({
       name: ['', Validators.compose([Validators.required])],
       address: ['', Validators.compose([Validators.required])]
@@ -27,29 +27,18 @@ export class CreateEndpointPage {
         let endpoint: Endpoint = new Endpoint(this.endpointForm.value.name, this.endpointForm.value.address)
         await this.endpointProvider.create(endpoint)
         // Display succes in a toast
-        this.showToast(`Le point d'accès '${endpoint.name}' a été ajouté avec succès.`,3000,CONSTANTS.TOAST.TYPE_SUCCESS,false)
+        this.toastCustom.showToast(`Le point d'accès '${endpoint.name}' a été ajouté avec succès.`,3000,this.toastCustom.TYPE_SUCCESS,false)
         // Return to the previous page
         this.navCtrl.pop()
       } catch (error) {
         // Display error in a toast
-        this.showToast(error,10000,CONSTANTS.TOAST.TYPE_ERROR,true)
+        this.toastCustom.showToast(error,10000,this.toastCustom.TYPE_ERROR,true)
       }
     } else {
       // Display error in a toast
       if (this.endpointForm.controls.name.hasError('required') || this.endpointForm.controls.address.hasError('required')) {
-        this.showToast('Les champs doivent être remplis',10000,CONSTANTS.TOAST.TYPE_ERROR,true)
+        this.toastCustom.showToast('Les champs doivent être remplis',10000,this.toastCustom.TYPE_ERROR,true)
       }
     }
-  }
-
-  // Toast
-  showToast(msg: string, dur: number, type: string, closeBtn: boolean = false) {
-    let toast = this.toastCtrl.create({
-      message: msg,
-      duration: dur,
-      cssClass: type,
-      showCloseButton: closeBtn
-    });
-    toast.present()
   }
 }
