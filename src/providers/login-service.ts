c1cf69509be91de6160c388eeb30a088e44bde2c
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-01-11 10:58:18 +0100

    Update login provider

--- src/providers/login-service.ts
+++ src/providers/login-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DataService } from "./data-service";
+import { Observable } from "rxjs";
 
 /*
   Generated class for the LoginServiceProvider provider.
@@ -11,19 +12,29 @@ import { DataService } from "./data-service";
 @Injectable()
 export class LoginService {
 
+  private _userlogged = false;
+  private _teams 
+  private _user;
+
   constructor(private dataService: DataService, private http: HttpClient) {
     this.dataService = dataService;
   }
 
   // Get the Login url of the current endpoints
-  getLoginUrl(){
+  getLoginUrl() {
     return this.dataService.getServeUrl() + "/login"
   }
 
-  checkIfLogged(){
-    let url = this.dataService.getServeUrl() + "/profil"
-    let data = this.dataService.getApiJson("/profil")
-    console.log(data)
+  // check if user is already logged
+  checkIfLogged() {
+    const o1 = this.dataService.getApiJson("/profil").do(data => {
+      console.log(data)
+      this._teams = data["teams"]
+      this._user = data["user"]
+    })
+    //console.log(`user ${this._user} teams ${this._teams}`)
+    Observable.forkJoin(o1).subscribe()
+    
   }
 
 }
