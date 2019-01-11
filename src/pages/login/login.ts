7031dfb2b9e050639fe190bcb259eb84ac1d1ffb
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-01-11 10:58:08 +0100

    Check url (temp url)

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -17,23 +17,68 @@ import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser'
 export class LoginPage {
 
   url: string;
+  loginUrlDomain: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private inAppBrowser: InAppBrowser, private loginProvider: LoginService) {
-    this.loginProvider.checkIfLogged()
     this.url = this.loginProvider.getLoginUrl()
-    this.openWebpage(this.url)
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  openwebbrowse() {
+    //this.openWebpage(this.url)
+    this.openWebpage("http://joutes.mycpnv.ch/saml2/login")
   }
 
   openWebpage(url: string) {
+    //Set browser options
     const options: InAppBrowserOptions = {
       zoom: 'no'
     }
-    console.log(url)
-    const browser = this.inAppBrowser.create(url, '_system', options)
+
+    //Open browse
+    const browser = this.inAppBrowser.create(url, '_self', options)
+
+    //reset the loginUrlDomain (know when user change page)
+    this.loginUrlDomain = null
+
+    //event listner when the page finish loaded
+    browser.on('loadstop').subscribe(event => {
+      if(this.loginUrlDomain == null){
+        this.loginUrlDomain = this.extractHostname(event.url)
+      }
+      else if (this.extractHostname(event.url) != this.loginUrlDomain ){
+        //if change close
+        browser.close()
+      }
+    });
+
   }
+
+  // Check if login successfull
+  checklogin(){
+    return this.loginProvider.checkIfLogged()
+  }
+
+  disconnect(){
+    this.openWebpage("http://joutes.mycpnv.ch/saml2/logout")
+  }
+
+  extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("//") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
 
 }
