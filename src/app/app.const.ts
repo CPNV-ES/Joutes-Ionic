81b679578270bd68b0a615fc96e68a0b599586ed
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-01-24 10:06:43 +0100

    Change default api

--- src/app/app.const.ts
+++ src/app/app.const.ts
@@ -3,5 +3,5 @@ import { Endpoint } from "../models/endpoint";
 export const GLOBAL = {
     apiVersion : '2.2.0',
     apiRequirements: ['name','version','api_routes'],
-    apiDefault: new Endpoint('Joutes CPNV', 'http://joutes.test/api', Endpoint.TYPE_OFFICIAL),
+    apiDefault: new Endpoint('Joutes CPNV', 'http://joutes.mycpnv.ch/api', Endpoint.TYPE_OFFICIAL),
 }
