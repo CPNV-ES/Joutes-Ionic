ee5999d3de7356ff7125382a467ba6dcc3c24931
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-02-06 11:45:16 +0100

    Manage local storage for offline use

--- src/providers/event.ts
+++ src/providers/event.ts
@@ -2,16 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Event } from '../models/event'
 import { EndpointProvider } from './endpoint';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class EventProvider {
 
   private events: Array<Event> = []
+  private keyName: string = 'events'
 
-  constructor(private http: HttpClient, private endpointProvider: EndpointProvider) {
+  constructor(private http: HttpClient, private endpointProvider: EndpointProvider, private storage: Storage) {
     
   }
   
+  // Get data from api
   private async getAllFromAPI() {
     if (await this.endpointProvider.isReady())
     {
@@ -19,8 +22,34 @@ export class EventProvider {
     }
   }
 
-  async getAll() {
-    let data = <any> await this.getAllFromAPI()
-    return data.events
+  // Get data from local storage
+  private async getAllFromLocal() {
+    if (await this.endpointProvider.isReady()) {
+      return this.storage.get(this.keyName)
+    }
+  }
+
+  async getAll(isOnline: boolean) {
+    let data: any = []
+
+    // Take the good provider
+    if (isOnline) {
+      data = await this.getAllFromAPI()
+      // Test if events exists
+      if (data != null && 'events' in data) {
+        data = data.events
+        // Save in the local storage
+        this.setLocalData(data)
+      } else {
+        data = []
+      }
+    } else {
+      data = await this.getAllFromLocal()
+    }
+    return data
+  }
+
+  private setLocalData(events: Array<Event>) {
+    return this.storage.set(this.keyName,events)
   }
 }
