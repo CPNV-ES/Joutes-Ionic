4fea4372240578f65144466382b3a4cf1d8a4e2c
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-02-15 13:33:09 +0100

    Add loader when we get events

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, Refresher } from 'ionic-angular';
+import { NavController, Refresher, LoadingController, Loading } from 'ionic-angular';
 import { SharedDataService } from '../../providers/sharedData-service';
 import { EventPage } from "../event/event";
 import { EventProvider } from '../../providers/event';
@@ -13,13 +13,16 @@ import { EndpointProvider } from '../../providers/endpoint';
 export class EventsPage implements OnInit {
     private events: Array<Event> = []
     private ready: boolean = false
+    private loader: Loading
 
-    constructor(private toastCustom: ToastCustom, private navCtrl: NavController, private sharedDataProvider: SharedDataService, private eventProvider: EventProvider, private endpointProvider: EndpointProvider) {
+    constructor(private loadingCtrl: LoadingController, private toastCustom: ToastCustom, private navCtrl: NavController, private sharedDataProvider: SharedDataService, private eventProvider: EventProvider, private endpointProvider: EndpointProvider) {
     
     }
 
     async ngOnInit() {
+        this.showLoader()
         await this.getData()
+        this.loader.dismiss()
         this.ready = true
         if (this.events != null && this.events.length == 1) {
             this.goToEvent(this.events[0])
@@ -45,5 +48,12 @@ export class EventsPage implements OnInit {
     goToEvent(event) {
         this.sharedDataProvider.currentEvent = event;
         this.navCtrl.push(EventPage);
+    }
+
+    async showLoader() {
+        this.loader = this.loadingCtrl.create({
+           content: "Veuillez patienter...<br/>Chargement des données en cours.",
+         });
+         this.loader.present()
     }
 }
